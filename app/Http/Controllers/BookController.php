c46522674cb4f19052817f8274b609ac53fecc32
rodgaravi <rgarcia@atid.edu.mx>
2021-07-21 19:42:49 -0500

    Book controller was created

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Client\Request;
+
+class BookController extends Controller
+{
+    // Standar responses
+    //use ApiResponser;
+
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Return a Book's list
+     * @return Illuminate\Http\Response
+     */
+    public function booksList()
+    {
+      
+    }
+
+    /**
+     * Create an instance of Book
+     * @return Illuminate\Http\Response
+     */
+    public function storeBook(Request $request){
+       
+    }
+
+
+    /**
+     * Return an specific Book
+     * @return Illuminate\Http\Response
+     */
+    public function getBook($book){
+       
+    }
+
+    /**
+     * Update an especific book data
+     * @return Illuminate\Http\Response
+     */
+    public function updateBook(Request $request, $book){
+       
+    }
+
+    /**
+     * Delete an specific book data
+     * @return Illuminate\Http\Response
+     */
+    public function deleteBook($book){
+       
+    }
+}
